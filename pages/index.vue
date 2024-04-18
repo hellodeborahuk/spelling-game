cb6b27d06cdabcc85a6cfbf7cfadbb8ddf174548
Debbie Dann <29425781+hellodeborahuk@users.noreply.github.com>
2024-04-18 16:59:54 +0100

    x if enter wrong with return key

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,23 @@
 <template>
     <div class="mx-auto max-w-screen-sm bg-slate-800 text-white m-6 px-6 lg:px-0 rounded-lg">
-        <div class="p-6 space-y-6 text-center">
-            <h1>Year 2 Spellings</h1>
-            <p>Look Cover Write Check</p>
-        <div class="text-6xl">
-            <template v-if="!isComplete">
-                <span v-if="wordVisible">{{ word }}</span>
-                <span v-else class="tracking-wider">{{ hiddenWord }}</span>
-            </template>
-                <span class="text-amber-400" v-else><v-icon name="gi-party-popper" class="w-14 h-14 text-pink-400" />Well done!<v-icon name="gi-party-popper" class="w-14 h-14 text-pink-400 scale-x-[-1]" /></span>
+        <div class="p-6 text-center">
+            <h1 class="text-3xl text-amber-400 font-bold mb-2">Year 2 Spellings</h1>
+            <p class="tracking-wide text-2xl font-medium mb-6">Look Cover Write Check</p>
+            <div class="text-6xl border border-pink-400 py-8 rounded-lg mb-6">
+                <template v-if="!isComplete">
+                    <span v-if="wordVisible">{{ word }}</span>
+                    <span v-else class="tracking-wider">{{ hiddenWord }}</span>
+                </template>
+                    <span class="text-amber-400" v-else><v-icon name="gi-party-popper" class="w-14 h-14 text-pink-400" />Well done!<v-icon name="gi-party-popper" class="w-14 h-14 text-pink-400 scale-x-[-1]" /></span>
+            </div>
+            <form @submit.prevent="nextWord" class="flex justify-between gap-10">
+                <input v-model="answer" class="w-2/3 py-3 text-4xl text-slate-800 rounded-lg px-4">
+                <span v-if="isCorrect"><v-icon name="fa-check" fill="currentColor" class="text-pink-400 w-14 h-14"/></span>
+                <span v-else-if="wrongAnswer"><v-icon name="fa-times" fill="currentColor" class="text-pink-400 w-14 h-14"/></span>
+                <button :disabled="!isCorrect" @click="nextWord" type="button" class="bg-amber-400 rounded-lg px-4 text-slate-800 text-2xl font-semibold disabled:bg-amber-400/50 hover:bg-pink-400 ease-in duration-150">next</button>
+            </form>
+            <button @click="playAgain" v-if="isComplete" class="bg-pink-400 rounded-lg px-4 py-2 text-slate-800 text-2xl font-semibold hover:bg-amber-400 ease-in duration-150">play again <v-icon name="md-replay" class="w-6 h-6" /></button>
         </div>
-        <form @submit.prevent="nextWord" class="flex justify-between gap-10">
-            <input v-model="answer" class="w-2/3 py-3 text-4xl text-slate-800">
-            <span v-if="isCorrect"><v-icon name="fa-check" fill="currentColor" class="text-pink-400 w-14 h-14"/></span>
-            <button :disabled="!isCorrect" @click="nextWord" type="button" class="bg-amber-400 rounded-lg px-4 text-slate-800 text-2xl font-semibold disabled:bg-amber-400/50 hover:bg-pink-400 ease-in duration-150">next</button>
-        </form>
-        <button @click="playAgain" v-if="isComplete" class="bg-pink-400 rounded-lg px-4 py-2 text-slate-800 text-2xl font-semibold hover:bg-amber-400 ease-in duration-150">play again <v-icon name="md-replay" class="w-6 h-6" /></button>
-    </div>
        
     </div>
 </template>
@@ -27,6 +28,7 @@
     const word = ref('')
     const done = ref([])
     const wordSet = ref([])
+    const wrongAnswer = ref(false)
 
     const hiddenWord = computed(() => {
         return '_'.repeat(word.value.length)
@@ -56,8 +58,10 @@
 
     const nextWord = () => {
         if (!isCorrect.value) {
+            wrongAnswer.value = true
             return
         }
+        wrongAnswer.value = false
         done.value.push(answer.value)
         answer.value=''
         if (done.value.length !== wordSet.value.length) {
--- plugins/oh-vue-icons.js
+++ plugins/oh-vue-icons.js
@@ -1,7 +1,7 @@
 import { OhVueIcon, addIcons } from "oh-vue-icons";
-import { FaCheck, GiPartyPopper, MdReplay } from "oh-vue-icons/icons";
+import { FaCheck, GiPartyPopper, MdReplay, FaTimes  } from "oh-vue-icons/icons";
 
-addIcons(FaCheck, GiPartyPopper, MdReplay);
+addIcons(FaCheck, GiPartyPopper, MdReplay, FaTimes );
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component("v-icon", OhVueIcon);
