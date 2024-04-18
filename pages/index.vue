c8ffaa19d125a0cc441a2c92280a0c5dbe249384
Debbie Dann <29425781+hellodeborahuk@users.noreply.github.com>
2024-04-18 16:17:33 +0100

    logic and words added to js file

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,19 @@
 <template>
-    <div class="mx-auto max-w-screen-sm bg-teal-200 px-6 lg:px-0">
+    <div class="mx-auto max-w-screen-sm bg-teal-200 m-6 px-6 lg:px-0 rounded-lg">
         <div class="p-6 space-y-6 text-center">
             <h1>Year 2 Spellings</h1>
             <p>Look Cover Write Check</p>
         <div class="text-6xl">
+            <template v-if="!isComplete">
                 <span v-if="wordVisible">{{ word }}</span>
                 <span v-else class="tracking-wider">{{ hiddenWord }}</span>
+            </template>
+                <span v-else>Well done!</span>
         </div>
-        <form @submit.prevent="onSubmit" class="flex justify-between gap-10">
+        <form @submit.prevent="nextWord" class="flex justify-between gap-10">
             <input v-model="answer" class="w-full py-3 text-4xl">
-            <button type="submit" class="bg-teal-800 text-white px-8 rounded-lg">Check</button>
+            <span v-if="isCorrect">tick</span>
+            <button :disabled="!isCorrect" @click="nextWord" type="button">Next</button>
         </form>
     </div>
        
@@ -17,35 +21,50 @@
 </template>
 
 <script setup>  
-    const words = [   
-        "after",
-        "again",
-        "any",
-        "bath",
-        "beautiful",
-        "because"
-    ];
+    import {words} from '../data/words'
 
     const word = ref('')
+    const done = ref([])
+    const wordSet = ref([])
 
     const hiddenWord = computed(() => {
         return '_'.repeat(word.value.length)
     })
 
     const randomWord = () => {
-        return words[Math.floor(Math.random()*words.length)]
+        const incompleteWords = wordSet.value.filter((w) => {
+            return done.value.indexOf(w) === -1
+        })
+        return incompleteWords[Math.floor(Math.random()*incompleteWords.length)]
     }
     onMounted(() => {
+        wordSet.value = words.sort(() => Math.random() - 0.5).slice(0,6)
         word.value = randomWord()
     })
 
     const answer = ref('')
     
-    const wordVisible = computed(() => {
-        return answer.value.length === 0
+    const isCorrect = computed(() => {
+        return answer.value.trim() === word.value
     })
-    const onSubmit = () => {
-        console.log(answer.value)
-    }
 
+    const wordVisible = computed(() => {
+        return answer.value.length === 0 || isCorrect.value
+    })
+
+    const nextWord = () => {
+        if (!isCorrect.value) {
+            return
+        }
+        done.value.push(answer.value)
+        answer.value=''
+        if (done.value.length !== wordSet.value.length) {
+        word.value = randomWord()
+        }
+    }
+    
+    const isComplete = computed(() => {
+        return word.value.length > 0 && done.value.length === wordSet.value.length
+    })
+   
 </script>
